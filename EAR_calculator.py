53434a9e99ebf46a47d43772b72f45e3dfe64a0d
SOUVIK GHOSH <35571958+fear-the-lord@users.noreply.github.com>
2020-07-09 13:53:17 +0530

    Add files via upload

---
+++ EAR_calculator.py
@@ -0,0 +1,12 @@
+# Define the function for calculating the Eye Aspect Ratio(EAR)
+from scipy.spatial import distance as dist 
+def eye_aspect_ratio(eye):
+	# Vertical eye landmarks
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+	# Horizontal eye landmarks 
+	C = dist.euclidean(eye[0], eye[3])
+
+	# The EAR Equation 
+	EAR = (A + B) / (2.0 * C)
+	return EAR
