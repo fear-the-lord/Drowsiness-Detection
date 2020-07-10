665869264ce2fbd7426a264c7baa27b9901f0bbb
SOUVIK GHOSH <35571958+fear-the-lord@users.noreply.github.com>
2020-07-10 19:34:29 +0530

    Add files via upload

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ def function2():
 	exit() 
 	
 root.title("DROWSINESS DETECTION")
-Label(root, text="DROWSINESS DETECTION",font=("times new roman",20),fg="white",bg="maroon",height=2).grid(row=2,rowspan=2,columnspan=5,sticky=N+E+W+S,padx=5,pady=10)
+Label(root, text="DROWSINESS DETECTION",font=("times new roman",20),fg="black",bg="aqua",height=2).grid(row=2,rowspan=2,columnspan=5,sticky=N+E+W+S,padx=5,pady=10)
 Button(root,text="Run",font=("times new roman",20),bg="#0D47A1",fg='white',command=function1).grid(row=5,columnspan=5,sticky=W+E+N+S,padx=5,pady=5)
 Button(root,text="Exit",font=("times new roman",20),bg="#0D47A1",fg='white',command=function2).grid(row=7,columnspan=5,sticky=W+E+N+S,padx=5,pady=5)
 
