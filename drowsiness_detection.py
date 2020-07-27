a0470bf904b509d572e61b2f68b1c66c168545ca
Sawon Bhattacharya <35263795+SawonBhattacharya@users.noreply.github.com>
2020-07-27 20:45:38 +0530

    updated with graph

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -1,7 +1,14 @@
 # Import the necessary packages 
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from EAR_calculator import *
 from imutils import face_utils 
-from imutils.video import VideoStream 
+from imutils.video import VideoStream
+import matplotlib.pyplot as plt
+import matplotlib.animation as animate
+from matplotlib import style 
+
 import imutils 
 import dlib
 import time 
@@ -10,14 +17,26 @@ import cv2
 from playsound import playsound
 from scipy.spatial import distance as dist
 import os 
+import csv
+import numpy as np
+import pandas as pd
 from datetime import datetime
 
+style.use('fivethirtyeight')
 # Creating the dataset 
 def assure_path_exists(path):
     dir = os.path.dirname(path)
     if not os.path.exists(dir):
         os.makedirs(dir)
 
+
+#all eye  and mouth aspect ratio with time
+ear_list=[]
+total_ear=[]
+mar_list=[]
+total_mar=[]
+ts=[]
+total_ts=[]
 # Construct the argument parser and parse the arguments 
 ap = argparse.ArgumentParser() 
 ap.add_argument("-p", "--shape_predictor", required = True, help = "path to dlib's facial landmark predictor")
@@ -54,7 +73,7 @@ assure_path_exists("dataset/")
 count_sleep = 0
 count_yawn = 0 
 
-
+ 
 # Now, loop over all the frames and detect the faces
 while True: 
 	# Extract a frame 
@@ -88,7 +107,12 @@ while True:
 
 		# Take the average of both the EAR
 		EAR = (leftEAR + rightEAR) / 2.0
+		#live datawrite in csv
+		ear_list.append(EAR)
+		#print(ear_list)
+		
 
+		ts.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
 		# Compute the convex hull for both the eyes and then visualize it
 		leftEyeHull = cv2.convexHull(leftEye)
 		rightEyeHull = cv2.convexHull(rightEye)
@@ -98,7 +122,7 @@ while True:
 		cv2.drawContours(frame, [mouth], -1, (0, 255, 0), 1)
 
 		MAR = mouth_aspect_ratio(mouth)
-
+		mar_list.append(MAR/10)
 		# Check if EAR < EAR_THRESHOLD, if so then it indicates that a blink is taking place 
 		# Thus, count the number of frames for which the eye remains closed 
 		if EAR < EAR_THRESHOLD: 
@@ -128,13 +152,41 @@ while True:
 			cv2.imwrite("dataset/frame_yawn%d.jpg" % count_yawn, frame)
 			playsound('sound files/alarm.mp3')
 			playsound('sound files/warning_yawn.mp3')
-			
-	#Display the frame 
+	#total data collection for plotting
+	for i in ear_list:
+		total_ear.append(i)
+	for i in mar_list:
+		total_mar.append(i)			
+	for i in ts:
+		total_ts.append(i)
+	#display the frame 
 	cv2.imshow("Output", frame)
 	key = cv2.waitKey(1) & 0xFF 
+	
+	
 
 	if key == ord('q'):
 		break
 
+a = total_ear
+b=total_mar
+c = total_ts
+
+df = pd.DataFrame({"EAR" : a, "MAR":b,"TIME" : c})
+df.to_csv("op_webcam.csv", index=False)
+df=pd.read_csv("op.csv")
+
+
+
+
+
+df.plot(x='TIME',y=['EAR','MAR'])
+#plt.xticks(rotation=45, ha='right')
+
+plt.subplots_adjust(bottom=0.30)
+plt.title('EAR & MAR calculation over time of webcam')
+plt.ylabel('EAR & MAR')
+plt.gca().axes.get_xaxis().set_visible(False)
+plt.show()
 cv2.destroyAllWindows()
 vs.stop()
